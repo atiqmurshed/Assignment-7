8192b5462ec48b8625d266cfeebab17bbef25d15
atiqmurshed <atiqmursherd631@gmail.com>
2024-10-25 22:30:44 -0400

    added a new project file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+      <h1 className="text-4xl bg-slate-400">Assignment-7: </h1>
+     
+      
+    </>
+  )
+}
+
+export default App
